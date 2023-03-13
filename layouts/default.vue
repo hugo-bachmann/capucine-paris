d152ef991c585af9e371c1c1d97d4be72cd7bd8e
unknown <contact@hugobachmann.fr>
2023-03-13 16:25:17 +0100

    [INIT]

---
+++ layouts/default.vue
@@ -0,0 +1,50 @@
+<script>
+    export default {
+        methods: {
+            toggleMenu() {
+                const menu = document.querySelector ('#menu')
+                menu.classList.toggle ('hiddenMobile')
+
+                const burger = document.querySelector ('.burger')
+                burger.classList.toggle ('active')
+            }
+        }
+    }
+</script>
+
+<template>
+    <header id="header">
+        <nav id="menu" class="hiddenMobile">
+            <ul>
+                <li>
+                    <a href="/">Accueil</a>
+                </li>
+                <li>
+                    <a href="/">Notre histoire</a>
+                </li>
+                <li>
+                    <a href="/">Gallerie</a>
+                </li>
+                <li>
+                    <a href="/">Services</a>
+                </li>
+                <li>
+                    <a href="/">Contact</a>
+                </li>
+            </ul>
+        </nav>
+
+        <div class="burger" @click="toggleMenu">
+            <div></div>
+        </div>
+
+    </header>
+
+    <main id="body">
+        <slot />
+    </main>
+
+    <footer>
+        footer ici
+    </footer>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <section id="accueil" class="container">
+        <h1>Grapucine</h1>
+        <p>paris</p>
+        <a class="btn" href="/#contact">Visitez</a>
+        <span class="modelName">Model : Abir Hamdi</span>
+    </section>
+
+    <section id="notre-histoire">
+        <div class="imgContainer left">
+            <img src="../public/img/stepan-kulyk.png" alt="Stepan Kulyk">
+            <span class="modelName">Photo : Stepan Kulyk</span>
+        </div>
+        <div class="content">
+            <h2 class="hiddenDesktop"><span>Notre</span> Histoire</h2>
+            <p>
+                <strong>Paris</strong> est la ville de la mode par excellence et une destination incontournable pour tous les amoureux de la mode. Si vous cherchez à percer dans <strong>l'industrie de la mode</strong> en tant que mannequin, il n'y a pas de meilleur endroit pour commencer que dans une <strong>agence de mannequin</strong> à Paris.
+            </p>
+            <p>
+                Notre agence de mannequin située au cœur de <strong>Paris est fiere de representer</strong> les meilleurs modèles de la ville. Nous avons une équipe de professionnels expérimentés qui sont passionnés par leur travail et qui ont une <strong>connaissance approfondie</strong> de l'industrie de la mode.
+            </p>
+        </div>
+        <div class="imgContainer right">
+            <img src="../public/img/omid-armin.png" alt="Omid Armin">
+            <span class="modelName">Photo: Omid Armin </span>
+        </div>
+    </section>
+    
+    <section id="gallerie">
+        <div class="slider">
+
+        </div>
+        <div class="content">
+            <h2>Gallerie</h2>
+            <p>Nous travaillons avec des <strong>modeles</strong> de tous types et de tous âges.Que vous cherchiez des mannequins pour des <strong>defiles de mode</strong>, des séances photos, des publicités, des vidéos ou des <strong>événements spéciaux</strong>, nous avons les meilleurs talents pour vous aider à atteindre vos objectifs.</p>
+            <p>Nous nous engageons à aider nos mannequins à se <strong>developper dans leur carriere</strong>, en leur offrant une formation et des conseils personnalisés pour améliorer leur <strong>technique</strong> de marche, leur posture, leur expression faciale, leur pose et leur style.</p>
+        </div>
+    </section>
+</template>
